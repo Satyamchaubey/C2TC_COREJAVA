07a5b67f800b99dc3a65f3a54ee79680bbf77c9e
satyam546 <styam123ru@gmail.com>
2022-10-20 00:42:35 +0530

    first commit

---
+++ codejava/src/codejava/firstcc.java
@@ -0,0 +1,17 @@
+package codejava;
+
+public class firstcc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		if(a==7) {
+			System.out.println("true");
+		}
+		else {
+			System.out.println("not true");
+		}
+
+	}
+
+}
---
+++ codejava/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author john
+ *
+ */
+module codejava {
+}
