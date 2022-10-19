e94f70653f7a6b8146a392ab277d8d6e68516652
satyam546 <styam123ru@gmail.com>
2022-10-20 00:49:19 +0530

    second directory.java

---
+++ codejava/src/codejava/secondc.java
@@ -0,0 +1,14 @@
+package codejava;
+
+public class secondc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 787302738;
+		int b = 746377820;
+		 int sum = a + b;
+		 System.out.println(sum);
+
+	}
+
+}
